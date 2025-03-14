b1b739757bd21f983ce02ac8269bb839aee135d2
Raquelg93 <154551009+Raquelg93@users.noreply.github.com>
2025-03-14 11:03:34 -0400

    get-recommendations.js

--- netlify/functions/get-recommendations.js
+++ netlify/functions/get-recommendations.js
@@ -23,7 +23,7 @@ exports.handler = async function(event, context) {
     
     // Create prompt for OpenAI
     const prompt = `
-      Based on the following preferences, recommend 5 books:
+      Based on the following preferences, recommend 5 books in the same genre:
       
       Favorite books: ${favoriteBooks}
       ${favoriteAuthors ? `Favorite authors: ${favoriteAuthors}` : ''}
